efb798c20ab989e62cbfb2c8e99b82ec48605e5a
Héctor Mateu <hmateu.ortola@gmail.com>
2023-07-09 18:14:48 +0200

    feat: Configures the server to connect to the database

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const config = {
+  "development": {
+    "username": process.env.DEV_DB_USERNAME,
+    "password": process.env.DEV_DB_PASSWORD,
+    "database": process.env.DEV_DB_NAME,
+    "host": process.env.DEV_DB_HOST,
+    "dialect": process.env.DEV_DB_DIALECT,
+    "port": process.env.DEV_DB_PORT
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+module.exports = config;
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const db = require('./db');
 const PORT = process.env.DEV_API_PORT;
 
 app.use(express.json());
 
-app.listen(PORT,()=>{
-    console.log(`Servidor levantado y a la escucha en el puerto ${PORT}`);
-});
+db.then(
+    () => {
+        app.listen(PORT, () => {
+            console.log(`Servidor levantado correctamente y a la escucha en el puerto ${PORT}`);
+        });
+    }
+)
+    .catch(
+        error => console.error(`Ha habido un error conectándose a la base de datos`, error)
+    );
